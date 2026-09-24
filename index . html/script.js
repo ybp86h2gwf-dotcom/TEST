e8a98ce8a3cdc68b19480b6abd1035d6ea544e11
ybp86h2gwf-dotcom <ybp86h2gwf@privaterelay.appleid.com>
2026-09-24 22:43:46 +0530

    Add files via upload

---
+++ index . html/script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    const button = document.querySelector('button');
+
+    button.textContent = 'Let\'s learn together';
+    button.setAttribute('aria-label', 'Learning interest confirmed');
+}
